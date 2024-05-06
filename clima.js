2cc1c608512c77eeb5182b0a3798f26115fb8576
Alberto <btocarmona@gmail.com>
2024-05-06 00:10:04 -0300

    Primer Commit

--- clima.js
+++ clima.js
@@ -14,19 +14,19 @@ buscar.addEventListener('click',()=>{
         console.log(url)
         if (response.status != 404){
             const respuesta =await response.json();
-            document.querySelector('.ciudad').textContent=respuesta.name;
-            document.querySelector('.latitud').textContent=`Latitud: ${respuesta.coord.lat}`;
-            document.querySelector('.longitud').textContent=`Longitud: ${respuesta.coord.lon}`;
-            document.querySelector('.temperatura').textContent=`Temperatura: ${(respuesta.main.temp - KELVIN).toFixed(1)} Grados`;
-            document.querySelector('.sensacion').textContent=`Sensación: ${(respuesta.main.feels_like - KELVIN).toFixed(1)} Grados`;
-            document.querySelector('.temp_minima').textContent=`Mínima: ${(respuesta.main.temp_min - KELVIN).toFixed(1)} Grados`;
-            document.querySelector('.temp_maxima').textContent=`Máxima: ${(respuesta.main.temp_max - KELVIN).toFixed(1)} Grados`;
-            document.querySelector('.presion').textContent=`Presión: ${respuesta.main.pressure}`;
-            document.querySelector('.humedad').textContent=`Humedad: ${respuesta.main.humidity} %`;
-            document.querySelector('.visibilidad').textContent=`Visibilidad: ${respuesta.visibility / 1000} Km`;
-            document.querySelector('.velocidad').textContent=`Velocidad: ${respuesta.wind.speed} km/h`;
-            document.querySelector('.direccion').textContent=`Dirección: ${respuesta.wind.deg}`;
-            document.querySelector('.rafagas').textContent=`Ráfagas: ${respuesta.wind.gust} km/h`;
+            document.querySelector('.ciudad').textContent=`${respuesta.name}`;
+            document.querySelector('.latitud').innerHTML=`<img class="icono2" src="./img/check-mark.svg" alt=""> Latitud: ${respuesta.coord.lat}`;
+            document.querySelector('.longitud').innerHTML=`<img class="icono2" src="./img/check-mark.svg" alt=""> Longitud: ${respuesta.coord.lon}`;
+            document.querySelector('.temperatura').innerHTML=`<img class="icono2" src="./img/check-mark.svg" alt=""> Temperatura: ${(respuesta.main.temp - KELVIN).toFixed(1)} Grados`;
+            document.querySelector('.sensacion').innerHTML=`<img class="icono2" src="./img/check-mark.svg" alt=""> Sensación: ${(respuesta.main.feels_like - KELVIN).toFixed(1)} Grados`;
+            document.querySelector('.temp_minima').innerHTML=`<img class="icono2" src="./img/check-mark.svg" alt=""> Mínima: ${(respuesta.main.temp_min - KELVIN).toFixed(1)} Grados`;
+            document.querySelector('.temp_maxima').innerHTML=`<img class="icono2" src="./img/check-mark.svg" alt=""> Máxima: ${(respuesta.main.temp_max - KELVIN).toFixed(1)} Grados`;
+            document.querySelector('.presion').innerHTML=`<img class="icono2" src="./img/check-mark.svg" alt=""> Presión: ${respuesta.main.pressure}`;
+            document.querySelector('.humedad').innerHTML=`<img class="icono2" src="./img/check-mark.svg" alt=""> Humedad: ${respuesta.main.humidity} %`;
+            document.querySelector('.visibilidad').innerHTML=`<img class="icono2" src="./img/check-mark.svg" alt=""> Visibilidad: ${respuesta.visibility / 1000} Km`;
+            document.querySelector('.velocidad').innerHTML=`<img class="icono2" src="./img/check-mark.svg" alt=""> Velocidad: ${respuesta.wind.speed} km/h`;
+            document.querySelector('.direccion').innerHTML=`<img class="icono2" src="./img/check-mark.svg" alt=""> Dirección: ${respuesta.wind.deg}`;
+            document.querySelector('.rafagas').innerHTML=`<img class="icono2" src="./img/check-mark.svg" alt=""> Ráfagas: ${respuesta.wind.gust} km/h`;
 
         }else{
             alert('no se encontro la ciudad')
